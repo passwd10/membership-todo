060269621b90b51d50334364ab12069fe20cbdf5
passwd10 <parkinseo94@naver.com>
2020-09-23 17:09:19 +0900

    SPA로 동작을 변경해라
    url로 페이지를 이동하지 않고 페이지 내부의 view를 새로그려주도록 한다

--- client/src/app.js
+++ client/src/app.js
@@ -1,6 +1,30 @@
-import { router } from './router';
+import MainPage from './pages/MainPage';
+import LoginPage from './pages/LoginPage';
+import ErrorPage from './pages/ErrorPage';
+
+const routes = [
+  { path: '/', page: LoginPage },
+  { path: '/main', page: MainPage },
+  { path: '/login', page: LoginPage },
+];
+
+const router = (currentPath) => {
+  const app = document.getElementById('app');
+
+  while (app.hasChildNodes()) {
+    app.removeChild(app.firstChild);
+  }
+
+  for (const route of routes) {
+    if (route.path === currentPath) {
+      return app.appendChild(route.page());
+    }
+  }
+  return app.appendChild(ErrorPage());
+};
 
 (() => {
-  const $app = document.getElementById('app');
-  router($app);
+  router('/');
 })();
+
+export { router };
